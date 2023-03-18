c20918af60a44f309b93ab353c71740f3b97b3cf
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2023-03-18 15:36:03 +0300

    Add SmokeText implementation (no salt)

--- src/components/base/SmokeText.tsx
+++ src/components/base/SmokeText.tsx
@@ -1,5 +1,85 @@
 import * as React from "react";
+import { generateClassName, generateStyle } from "./utils";
 
 export default ({children}) => {
-	return children;
+	const text: string = children.normalize('NFC');
+
+	// Предупреждение 'item was found 2' (/м.*о.*с.*к.*в.*а/i.test(element.innerHTML)) 
+	// будет возникать всегда, но не из-за SmokeText а из-за обычного текста. Например:
+	// "мин. от м. Сходненская. Развитая инфраструктура. Индивидуа"
+
+	// 1) subtitute some chars to similar chars in different languages
+	// 2) split text in pieces and insert it in spans
+	// 3) generate salt
+
+	const obfuscateString = (text: string): string => {
+		// Используем только английские омоглифы, иначе появится большая
+		// вероятность возникновения проблем со шрифтом
+		const similarChars = {
+			'а': 'a',
+			'В': 'B',
+			'Е': 'E',
+			'е': 'e',
+			'К': 'K',
+			'М': 'M',
+			'Н': 'H',
+			'О': 'O',
+			'о': 'o',
+			'Р': 'P',
+			'р': 'p',
+			'с': 'c',
+			'С': 'C',
+			'Т': 'T',
+			'у': 'y',
+			'Х': 'X',
+			'х': 'x',
+		};
+
+		// replaceAll unavailable. Iterating over string 
+		const chars = Array.from(text);
+		for (let i = 0; i < chars.length; i++) {
+			if (similarChars.hasOwnProperty(chars[i])) {
+				chars[i] = similarChars[chars[i]];
+			}
+		}
+
+		return chars.join('');
+	}
+
+	const splitText = (text: string): string[] => {
+		const chars = Array.from(text);
+		const chunkSize = chars.length ** 0.5 >> 0;
+		
+		let chunks: string[] = [];
+
+		let i = 0;
+		while (i < chars.length) {
+			const chunk = chars.slice(i, i + chunkSize).join('');
+			chunks.push(chunk);
+			i += chunkSize;
+		}
+
+		return chunks;
+	}
+
+	const obfuscatedText = obfuscateString(text);
+	const sheet: {[key: string]: string} = {};
+	const elements = [];
+	
+	for (const chunk of splitText(obfuscatedText)) {
+		const className = generateClassName();
+		const pseudoClassName = `${className}::after`
+		sheet[`.${pseudoClassName}`] = `{
+			content: "${obfuscateString(chunk)}"
+		}`
+
+		elements.push(<span key={className} className={className}/>)
+	}
+
+
+	
+	return <>
+		<style>{generateStyle(sheet)}</style>
+		{elements}
+	</>
 };
--- src/components/base/utils.ts
+++ src/components/base/utils.ts
@@ -1,5 +1,17 @@
 import * as Cid from 'smokescreen/Cid'
 
+export const generateStyle = (sheet: {[key: string]: string}): string => {
+    let style: string = ''
+
+    for (const [key, value] of Object.entries(sheet)) {
+        if (value) {
+            style += `${key} ${value}`;
+        }
+    }
+
+    return style;
+}
+
 export const generateClassName = (): string => {
     return Cid();
 }
