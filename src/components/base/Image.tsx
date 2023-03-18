648142f6a00e1e978b4c07dab52c9dfa9e7de9a1
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2023-03-18 17:26:50 +0300

    Refactor

--- src/components/base/Image.tsx
+++ src/components/base/Image.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import {generateClassName, generateStyleString} from './utils'
+import {generateClassName, generateStyle} from './utils'
 
 
 type ImageProps = {
@@ -16,17 +16,19 @@ export default (props: ImageProps) => {
 	// RenderComponent, если я захочу использовать другие имена классов.
 	// Не уверен, что их можно изменять.
 
-	const styles = {
-		width: props.width + 'px',
-		height: props.height + 'px',
-		"background-image": `url(${props.src})`,
-	}
+	const className: string = generateClassName();
+	const sheet = {
+		['.' + className]: `{
+			width: ${props.width + 'px'};
+			height: ${props.height + 'px'};
+			background-image: ${`url(${props.src})`};
+		}`
+	};
 
-	const className: string = `${generateClassName()}`;
 	
 	return (
 		<>
-			<style>{generateStyleString(className, styles)}</style>
+			<style>{generateStyle(sheet)}</style>
 			<div className={className}/>
 		</>
 	);
--- src/components/base/utils.ts
+++ src/components/base/utils.ts
@@ -15,15 +15,3 @@ export const generateStyle = (sheet: {[key: string]: string}): string => {
 export const generateClassName = (): string => {
     return Cid();
 }
-
-export const generateStyleString  = (className: string, cssProperties: Object): string => {
-    const styleStrings: string[] = [];
-	for (const [key, value] of Object.entries(cssProperties)) {
-        if (value) {
-		    styleStrings.push(`${key}:${value}`);
-        }
-	}
-
-    return `.${className} {${styleStrings.join(';')}}`;
-}
-
